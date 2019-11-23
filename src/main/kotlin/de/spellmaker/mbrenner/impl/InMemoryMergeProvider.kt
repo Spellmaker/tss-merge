8f64b8a3f99ef72bf399087eeaf20deb4124e6fa
Markus Brenner <markus.brenner@uni-ulm.de>
2019-11-23 13:54:13 +0100

    added implementation

--- src/main/kotlin/de/spellmaker/mbrenner/impl/InMemoryMergeProvider.kt
+++ src/main/kotlin/de/spellmaker/mbrenner/impl/InMemoryMergeProvider.kt
@@ -1,7 +1,39 @@
 package de.spellmaker.mbrenner.impl
 
 import de.spellmaker.mbrenner.Interval
+import kotlin.math.max
 
+/**
+ * In memory implementation of {@link merge}
+ */
 fun mergeInMemory(input: List<Interval>): List<Interval> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    if (input.isEmpty()) {
+        return input
+    }
+    /* sort input by lower bound */
+    val sortedInput = input.sortedBy { it.first }
+
+    /* reduce elements via fold */
+    val state : Pair<MutableList<Interval>, Interval> =
+        sortedInput.fold(Pair(mutableListOf(), sortedInput.first()), ::mergeNext)
+
+    /* do not forget final element */
+    state.first.add(state.second)
+    return state.first
 }
+
+/**
+ * Helper function processing exactly one element
+ * @param state The current state with the first element containing the result and the second the current interval
+ * @param next The next interval to be considered
+ * @return The updated state
+ */
+private fun mergeNext(state : Pair<MutableList<Interval>, Interval>, next : Interval) =
+    if (state.second.second >= next.first) {
+        /* intervals overlap, merge intervals and update state*/
+        Pair(state.first, Interval(state.second.first, max(state.second.second, next.second)))
+    } else {
+        /* intervals do not overlap, add result and update state */
+        state.first.add(state.second)
+        Pair(state.first, next)
+    }
